efe25c67251453f1c38e38067d471701dae8e4a0
daeunblue <vkfksqlcgkxm@naver.com>
2022-02-14 21:41:58 +0900

    SWEA_D4_사칙연산유효성검사

---
+++ src/algorithm_practice/SWEA/SWEA_D4_사칙연산유효성검사.java
@@ -0,0 +1,61 @@
+package algorithm;
+
+import java.util.*;
+import java.io.*;
+
+public class SWEA_D4_사칙연산유효성검사 {
+	/*
+	 * node 가 숫자일때 , 자식을 가지고있으면 false 아니면 true
+	 */
+	static char[] node = new char[222];
+	static int n;
+	static int result;
+	public static void init() {
+		for (int i = 0; i < n; i++)
+			node[i] = 0;
+	}
+
+	
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+
+		for (int test_case = 1; test_case <= 10; test_case++) {
+			n = sc.nextInt();
+			init();
+			String s = sc.nextLine();
+			
+			for (int i = 1; i <= n; i++) {
+				s = sc.nextLine();
+				String[] arr = s.split(" ");
+				node[i] = arr[1].charAt(0);
+			}
+			
+			if(check(1) == true) {
+				result = 1;
+			}else {
+				result = 0;
+			}
+			
+			System.out.println("#" + test_case + " " + result);
+		}
+	}
+	
+	public static boolean isIn(int x) {
+		return (1 <= x && x <= n && node[x] == 0);
+	}
+
+	public static boolean check(int x) {
+		
+		if (x > n)
+			return false;
+		char c = node[x];
+		
+		if ('0' <= c && c <= '9') {	// 숫자일때 -> 자식 이 있는지 확인 
+			if ((isIn(x * 2) && isIn(x * 2 + 1)) || (x * 2 > n) && (x * 2 + 1 > n))
+				return true;
+			return false;
+		} else
+			return (check(x * 2) & check(x * 2 + 1));
+	}
+}
